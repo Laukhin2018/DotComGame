14c33cf8faed50d5fb5c2e1ca6a2b9b70d1e0823
Ilya <i.laukhin@hybrid.ru>
2020-01-13 12:42:10 +0300

    Test

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,35 +2,31 @@ package game;
 
 public class Game {
 
-    /*public String checkYourself(String stringGuess) {
+    int[] locationCell;
+    int numOfHits = 0;
+
+    public void setLocationCell(int[] locs){
+        locationCell = locs;
+    }
+
+    public String checkYourself(String stringGuess) {
 
         int guess = Integer.parseInt(stringGuess);
-
         String result = "Мимо";
 
         for (int cell : locationCell){
             if (guess == cell){
-
                 result = "Попал";
-
                 numOfHits++;
-
                 break;
             }
         }
 
         if (numOfHits == locationCell.length){
-
             result = "Потопил";
-
         }
 
         System.out.println(result);
-
         return result;
-    }*/
-
-    public static void main(String[] args) {
-        System.out.print("Хуй!!!");
     }
 }
